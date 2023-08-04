7d60d509d81a4be685fa7ce78d89a62c430990a9
Diana Buria <111434887+dianaburia@users.noreply.github.com>
2023-08-04 20:09:42 +0200

    Update travel_lab1.R

--- travel_lab1.R
+++ travel_lab1.R
@@ -3,57 +3,57 @@ library(moments)
 flights <- read.csv("C:/Users/Diana/datasets/flights.csv")
 View(flights)
 
-#Емпірична функція щільності
+#Empirical density function
 plot(ecdf(flights$price), xlab = "Price", ylab = "Fn(Price)")
 plot(ecdf(flights$time), xlab = "Time", ylab = "Fn(Time)")
 plot(ecdf(flights$time), xlab = "Distance", ylab = "Fn(Distance)")
 
-#Скринька з вусами
+#Boxplot
 boxplot(flights$price, col = "yellow", ylab = "Pressure")
 boxplot(flights$time, col = "red", ylab = "Pressure")
 boxplot(flights$distance, col = "green", ylab = "Pressure")
 
-#Мінімальне спостереження вибірки
+#Minimum
 min(flights$price)
 min(flights$time)
 min(flights$distance)
 
-#Максимальне спостереження вибірки
+#Maximum
 max(flights$price)
 max(flights$time)
 max(flights$distance)
 
-#Медіана
+#Median
 median(flights$price)
 median(flights$time)
 median(flights$distance)
 
-#Квартилі
+#Quartile
 quantile(flights$price)
 quantile(flights$time)
 quantile(flights$distance)
 
-#Децилі
+#Decile
 quantile(flights$price, p = seq(0, 1, 0.1))
 quantile(flights$time, p = seq(0, 1, 0.1))
 quantile(flights$distance, p = seq(0, 1, 0.1))
 
-#Матсподівання
+#Mathematical expectation
 mean(flights$price)
 mean(flights$time)
 mean(flights$distance)
 
-#Геометричне середнє
+#Geometric mean
 exp(mean(log(flights$price)))
 exp(mean(log(flights$time)))
 exp(mean(log(flights$distance)))
 
-#Середнє гармонічне
+#Harmonic mean
 1/mean(1/flights$price)
 1/mean(1/flights$time)
 1/mean(1/flights$distance)
 
-#Мода
+#Mode
 getmode <- function(x) {
   uniqx <- unique(x)
   uniqx[which.max(tabulate(match(x,uniqx)))]
@@ -65,50 +65,50 @@ getmode(flights$distance)
 
 
 
-#Дисперсія
+#Variance
 var(flights$price)
 var(flights$time)
 var(flights$distance)
 
-#Стандартне відхилення
+#Standart deviation
 sd(flights$price)
 sd(flights$time)
 sd(flights$distance)
 
-#Коефіцієнт варіацій
+#Coefficient of variation
 sd(flights$price) / mean(flights$price)
 sd(flights$time) / mean(flights$time)
 sd(flights$distance) / mean(flights$distance)
 
-#Стохастичне розсіювання
+#Stochastic dispersal
 (quantile(flights$price, 0.75) - quantile(flights$price, 0.25))/2
 (quantile(flights$time, 0.75) - quantile(flights$time, 0.25))/2
 (quantile(flights$distance, 0.75) - quantile(flights$distance, 0.25))/2
 
-#Розмах вибірки
+#Sample size
 max(flights$price) - min(flights$price)
 max(flights$time) - min(flights$time)
 max(flights$distance) - min(flights$distance)
 
-#Інтарвал концентрації
+#Interquartile range
 IQR(flights$price)
 IQR(flights$time)
 IQR(flights$distance)
 
-#Коефіцієнт кореляції Пірсона, аналіз істотності парних статистичних зв'язків
-cor.test(flights$price, flights$time, method = 'spearman')
-cor.test(flights$price, flights$distance, method = 'spearman')
-cor.test(flights$distance, flights$time, method = 'spearman')
+#Pearson Correlation Coefficient, analysis of the strength of a linear statistical relationship between paired data
+cor.test(flights$price, flights$time, method = 'pearson')
+cor.test(flights$price, flights$distance, method = 'pearson')
+cor.test(flights$distance, flights$time, method = 'pearson')
 cov(flights$price,flights$time)/sqrt(var(flights$price)*var(flights$time))
 cov(flights$price,flights$distance)/sqrt(var(flights$price)*var(flights$distance))
 cov(flights$distance,flights$time)/sqrt(var(flights$distance)*var(flights$time))
 
-#Максимальний рівень значущості при якому відповідний парний статистичний зв'язок не є значимим
+#The maximum level of significance, where the corresponding pairwise statistical relationship is not significant
 cor.test(flights$price,flights$time)$p.value
 cor.test(flights$price,flights$distance)$p.value
 cor.test(flights$distance,flights$time)$p.value
 
-#Коефіцієнт множинної кореляції, аналіз істотності множинних статистичних зв'язків
+#Multiple correlation coefficient, analysis of the strength of multiple statistical relationships
 #a <- flights$price
 #b <- flights$time
 #c <- flights$distance
@@ -133,7 +133,7 @@ View(newflights)
 model <- lm(flights$time~flights$distance+flights$price,data=flights)
 summary(model)
 
-pairs(newflights,col=3)                                    #матриця розсіювання
+pairs(newflights,col=3)                                    #plot matrix, consisting of scatter plots corresponding to each data frame
 
 a <- flights$time
 plot(model$fitted.values,newflights$a)
